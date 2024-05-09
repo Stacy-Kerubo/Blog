73c7be7f400745750a452a4e6910e32f488d76d4
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-05-09 17:53:44 +0300

    List and detail views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from . models import  Post
+
+
 
 # Create your views here.
+
+'''creating a view to display list of posts'''
+
+def post_list(request):
+    posts=Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                 {'posts': posts} )
+    
+'''creating a view to display a single post'''    
+def post_detail(request,id):
+    post=get_object_or_404(Post,
+                           id=id,
+                           status=Post.Status. PUBLISHED)
+    
+    return render(request,
+                   'blog/post/detail.html',
+                    {'post': post})
+    
