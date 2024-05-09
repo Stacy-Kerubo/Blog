49c067205409bd41afdeade7d64c2a817af27ad6
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-05-09 15:01:07 +0300

    models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -7,17 +8,19 @@ class Post(models.Model):
         ('draft','Draft'),
         ('published','Published'),
     )
-    title=models.CharField(max_length=250,Unique=True)
-    slug=models.SlugField(max_length=250,unique=True)
-    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_post')
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250)
+    content=models.TextField()
+    #author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_post')
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
+    publish=models.DateTimeField(default=timezone.now)
     status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='Draft')
-    content=models.TextField()
+ 
     
     
     class Meta:
-        ordering=('-created')
+        ordering=('-publish')
     def __str__(self):
         return self.title
        
